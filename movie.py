582ee3ca8db73928cc2f7335bdb0996f7bf0bae0
INFERNO <vicentecastro1205@gmail.com>
2024-03-08 11:35:39 -0500

    last update

--- movie.py
+++ movie.py
@@ -1,30 +1,53 @@
+import os
+import boto3
+import json
 import requests
 
-def buscar_peliculas(nombre_pelicula):
-    api_key = '1e464630617e4408ebd290e882b5ade7'
+def buscar_peliculas_lambda(nombre_pelicula):
+    api_key = os.environ.get('TMDB_API_KEY')
     base_url = 'https://api.themoviedb.org/3'
     endpoint = '/search/movie'
     url = f'{base_url}{endpoint}?api_key={api_key}&query={nombre_pelicula}'
+    
     response = requests.get(url)
+    
     if response.status_code == 200:
-        resultados = response.json()['results']
-        if resultados:
-            print("Películas encontradas:")
-            for pelicula in resultados:
-                print(f"Título: {pelicula['title']}, ID: {pelicula['id']}")
-        else:
-            print("No se encontraron películas con ese nombre.")
-            # Si no se encuentra ninguna película, pedir al usuario nuevamente el nombre
-            nombre_pelicula = input("Ingrese el nombre de la película (o 'q' para salir): ")
-            if nombre_pelicula.lower() != 'q':
-                buscar_peliculas(nombre_pelicula)
+        datos = response.json()
+        return datos
     else:
-        print("Hubo un error al realizar la solicitud a la API.")
+        return None
+
+def buscar_peliculas(nombre_pelicula):
+    resultados = buscar_peliculas_lambda(nombre_pelicula)
+    
+    if resultados:
+        print("Películas encontradas:")
+        for pelicula in resultados.get('results', []):
+            print(f"Título: {pelicula.get('title')}, ID: {pelicula.get('id')}")
+        codigo_pelicula = input("Ingrese el código de la película que desea obtener más información (o 'q' para salir): ")
+        if codigo_pelicula.lower() != 'q':
+            obtener_informacion_pelicula(codigo_pelicula)
+            buscar_otra_pelicula()
+    else:
+        print("No se encontraron películas con ese nombre.")
+        buscar_otra_pelicula()
+
+def obtener_informacion_pelicula(codigo_pelicula):
+    # Aquí puedes implementar la lógica para obtener información detallada de una película
+    pass
+
+def buscar_otra_pelicula():
+    respuesta = input("¿Desea buscar otra película? (s/n): ")
+    if respuesta.lower() == 's':
+        nombre_pelicula = input("Ingrese el nombre de la película: ")
+        buscar_peliculas(nombre_pelicula)
+    else:
+        print("Gracias por usar el programa.")
 
 def main():
-    nombre_pelicula = input("Ingrese el nombre de la película (o 'q' para salir): ")
-    if nombre_pelicula.lower() != 'q':
-        buscar_peliculas(nombre_pelicula)
+    nombre_pelicula = input("Ingrese el nombre de la película: ")
+    buscar_peliculas(nombre_pelicula)
 
 if __name__ == "__main__":
     main()
+
